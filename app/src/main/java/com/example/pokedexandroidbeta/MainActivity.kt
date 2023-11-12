63a559dac0348f3cf76bc24c9150561d9ed5d728
hamzahdiza <hamzah.diza@gmail.com>
2023-11-12 12:29:39 +0700

    Pokemon Detail 1

--- app/src/main/java/com/example/pokedexandroidbeta/MainActivity.kt
+++ app/src/main/java/com/example/pokedexandroidbeta/MainActivity.kt
@@ -17,9 +17,11 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.pokedexandroidbeta.pokemondetail.PokemonDetailScreen
 import com.example.pokedexandroidbeta.pokemonlist.PokemonListScreen
 import com.example.pokedexandroidbeta.ui.theme.PokedexAndroidbetaTheme
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -54,6 +56,12 @@ class MainActivity : ComponentActivity() {
                         val pokemonName = remember {
                             it.arguments?.getString("pokemonName")
                         }
+
+                        PokemonDetailScreen(
+                            dominantColor = dominantColor,
+                            pokemonName = pokemonName?.lowercase(Locale.ROOT) ?: "",
+                            navController = navController
+                        )
                     }
                 }
 
